b74c0983f1ff2a9a172118da7a07609211520b73
1orwell <elise@eliseratcliffe.co.uk>
2022-03-21 21:14:07 +0000

    Project 18.py

--- Project 18.py
+++ Project 18.py
@@ -30,32 +30,51 @@ permutations_of_tuples = []
 permutations_of_lists = []
 
 #Attempting to list all permutations of the indexes
-for element in list(itertools.permutations([1, 2, 3, 4, 5,6,7,8])):
+for element in list(itertools.permutations([0,1,2])):
     permutations_of_tuples.append(element)
 
 for tup in permutations_of_tuples:
     permutations_of_lists.append(list(tup))
 
+for perm_list in permutations_of_lists:
+    print(perm_list)
 
 #Removing any permutations that mean the value taken from the triangle wouldn't be agacent to the item above
+# I can hear my computer whir when it gets to this point
+# For some reason, this is skipping out the third possible permutation? Possible indexing error?
 for perm_list in permutations_of_lists:
     index=0
     previous_val = perm_list[0]
     current_val = perm_list[0]
+    indexing_check = 0
+    print("in list " + str(perm_list) + " of the list of permutations")
     for val in perm_list:
-        current_val = val
-        print("current value: " + str(current_val))
-        print("previous value: " + str(previous_val))
-        if current_val != previous_val or previous_val + 1 != current_val:
-            print("At index " + str(index) + "found list with none adjacent indexes")
-            print("Removed " + str(permutations_of_lists[index]))
+        print("val is "+str(val))
+        print("this should not be more than "+str(indexing_check))
+        if val>indexing_check:
+            print("removing this list")
+            indexing_check = 0 
             del permutations_of_lists[index]
-        previous_val = val
+            #index += 1
+            break
+        indexing_check += 1
     index += 1
 
 for perm_list in permutations_of_lists:
     print(perm_list)
+"""
+    index=0
+    for val in perm_list:
+        current_val = val
+        if current_val != previous_val or previous_val + 1 != current_val:
+            del permutations_of_lists[index]
+        previous_val = val
+        index += 1
 
+for perm_list in permutations_of_lists:
+    print(perm_list)
+
+"""
 """
 for row in triangle[1:]:
     val_a = row[previous_index]
