0f056fd64a6b489ed031d5687eede09ebcfd893c
1orwell <elise@eliseratcliffe.co.uk>
2022-04-06 12:09:37 +0100

    completed project 22

--- project22.py
+++ project22.py
@@ -1,10 +1,20 @@
 def edit_file(filename):
+    total = 0
     with open(filename, "r") as filestream:
         with open('answers.txt', "w") as filestream2:
             for line in filestream:
                 current_line = line.split(',')
                 sorted_line = sorted(current_line)
-                for name in sorted_line:
-                    filestream2.write(name)
+                for i, name in enumerate(sorted_line):
+                    i = i+1
+                    score = 0
+                    for character in name[1:-1]:
+                        val = ord(character.lower()) - 96
+                        score += val
+                    result = score * i
+                    print(f'The result for {name} in the dict is {result}')
+                    total += result
+                    filestream2.write(str(score))
+    print(total)
 
 print(edit_file('p022_names.txt'))
