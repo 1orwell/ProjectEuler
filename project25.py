0466cd903391625c5f1c8bd181baad32c6eb49bc
1orwell <elise@eliseratcliffe.co.uk>
2022-04-10 12:05:40 +0100

    success

--- project25.py
+++ project25.py
@@ -2,11 +2,15 @@ from math import sqrt
 from decimal import *
 
 def fib(n):
-    getcontext.prec = 2000
-    return Decimal(((1+sqrt(5))**n - (1-sqrt(5))**n)/(2**n * sqrt(5)))
+    # use Decimal precision to be able to use enough digits
+    getcontext().prec = 2000
+    # Can't use recursion as reach recursion limit very quickly
+    return ((Decimal(1)+Decimal(sqrt(5)))**Decimal(n) - (Decimal(1)-Decimal(sqrt(5)))**Decimal(n))/(Decimal(2)**Decimal(n) * Decimal(sqrt(5)))
 
-for i in range(900):
-    print(i, fib(i))
-    if len(str(fib(i))) > 1000:
+for i in range(100000000000000):
+    # round to get rid of decimals before finding length
+    num = round(fib(i))
+    #print(i, num, len(str(num)))
+    if len(str(num)) == 1000:
         print(i)
         break
