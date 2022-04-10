c663694f52bf53b2af613d28df805560b0862f19
1orwell <elise@eliseratcliffe.co.uk>
2022-04-10 20:44:54 +0100

    completed

--- project26.py
+++ project26.py
@@ -1,4 +1,5 @@
 from decimal import *
+import math
 
 # denominator will never change
 # numerator will be the new remainder each time
@@ -13,7 +14,9 @@ def long_div(answer, numer, denom, check):
         answer.append(0) 
         return long_div(answer, numer*10, denom, check)
     else:
-        multiple = round(numer/denom)
+        # don't want to 'round' as that could round up, e.g. saying that 7 goes into 60 
+        # 9 times instead of 8
+        multiple = math.floor(numer/denom)
         answer.append(multiple)
         new_numer = (numer % denom) * 10
         if new_numer in check:
@@ -24,8 +27,23 @@ def long_div(answer, numer, denom, check):
             check.append(new_numer)
             return long_div(answer, new_numer, denom, check)
 
-print(long_div([], 1, 7, []))
+dec_map = {}
+tmp = [] 
+index = 0
 
-for i in range(1,10):
+for i in range(1,1000):
     rep_lst = long_div([], 1, i, [])
-    print(f'1/{i} is {rep_lst}')
+    #print(f'1/{i} is {rep_lst}')
+    if len(rep_lst) > len(tmp):
+        tmp = rep_lst
+        index = i
+        dec_map[i] = rep_lst
+
+print(f'tmp is {tmp}')
+print(f'dec_map is {dec_map}')
+print(f'index is {index}')
+print(f'Final answer is {dec_map[index]}')
+
+
+        
+
